07f5dce35e3baf0b6e866833fea58643d3068919
remi.odufuye <remi.odufuye@gmail.com>
2019-10-24 15:57:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,9 +53,9 @@ end
 
 #Remember, all reduce methods return a value.
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0 )
  
-   new = starting_point
+  new = starting_point
   i = 0
   while i < source_array.length do
     new += source_array[i]
